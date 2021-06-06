ced3031096952dbbddd4f5face587d8e4ba60fbb
mhiratani <m.malacca@outlook.com>
2021-06-06 14:34:04 +0900

    edit

--- index.js
+++ index.js
@@ -7,18 +7,52 @@ const client = new twitter({
   access_token_secret: process.env.access_token_secret
 });
 
-exports.handler = async () => {
-  const params = { screen_name: "milunhima", count: 1 };
-  await client
-    .get("favorites/list", params)
-    .then((tweet) => {
-      console.log(tweet);
-    })
-    .catch((error) => {
-      throw error;
-    });
+exports.handler = async (event) => {
+  const params = { screen_name: event.queryStringParameters.name, count: 180 };
+  if (event.queryStringParameters.maxid) {
+    params.max_id = Number(event.queryStringParameters.maxid);
+  }
+  const tweets = await getTweets(params);
   return {
     statusCode: 200,
-    body: "success!"
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "OPTIONS,GET",
+    },
+    body: JSON.stringify(tweets),
   };
+};
+
+const getTweets = function (params) {
+  return new Promise((resolve, reject) => {
+    client.get("favorites/list", params, function (error, tweets, response) {
+      if (!error) {
+        resolve(extractData(tweets));
+      } else {
+        console.error(error);
+        reject(error);
+      }
+    });
+  });
+};
+
+const extractData = function (tweets) {
+  var images = { url: [], source: [], height: [], max_id: 0 };
+  tweets.forEach((tweet) => {
+    if (tweet.entities.media) {
+      if (tweet.entities.media[0].type == "photo") {
+        if (!tweet.entities.media[0].media_url_https.includes("video_thumb")) {
+          images.url.push(tweet.entities.media[0].media_url_https);
+          images.source.push(tweet.entities.media[0].expanded_url);
+          const w = tweet.entities.media[0].sizes.medium.w;
+          const h = tweet.entities.media[0].sizes.medium.h;
+          images.height.push(h / w);
+        }
+      }
+    }
+  });
+  const max_id = tweets[tweets.length - 1].id - 10000;
+  images.max_id = max_id;
+  return images;
 };
